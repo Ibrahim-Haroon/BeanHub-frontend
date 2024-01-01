ffc397ecf36fd534e51de2b0cddc3f77fd5539ec
Ibrahim Haroon <99413953+Ibrahim-Haroon@users.noreply.github.com>
2024-01-01 10:59:47 -0500

    updated import for Order

--- tests/Order.test.js
+++ tests/Order.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Order from '../src/components/Order';
+import Order from '../components/Order';
 
 describe('Order', () => {
   it('renders without crashing', () => {
