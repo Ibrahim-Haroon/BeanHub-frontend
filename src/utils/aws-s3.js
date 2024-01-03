aa0ff1dcbdad68db4123c2d20c53657c6b07c340
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-03 16:00:09 -0500

    updated code, bare minumum for MVP.

---
+++ src/utils/aws-s3.js
@@ -0,0 +1,25 @@
+// s3.js
+import AWS from 'aws-sdk';
+
+export const uploadToS3 = async (file) => {
+    AWS.config.update({
+        region: process.env.REACT_APP_AWS_REGION,
+        accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+    });
+
+    const s3 = new AWS.S3();
+    const params = {
+        Bucket: process.env.REACT_APP_S3_BUCKET,
+        Key: 'customer_order.wav', // a unique file name
+        Body: file,
+        ContentType: 'audio/wav',
+    };
+
+    try {
+        await s3.upload(params).promise();
+        console.log('File uploaded successfully');
+    } catch (error) {
+        console.error(error);
+    }
+};
