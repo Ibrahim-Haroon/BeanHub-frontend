0e2721ceca4bdb39e156c651942081a3b6c69575
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-06 18:11:09 -0500

    removed uneeded code.

--- src/components/MicIcon.js
+++ src/components/MicIcon.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import styles from '../styles/MicIcon.module.css';
-import mic from './images/microphone-icon.png'
+import mic from '../images/microphone-icon.png'
 
 
 const MicIcon = ({ recording }) => {
     return (
         <div className={styles.waveformContainer}>
             <img src={mic} alt="Microphone" className={styles.microphoneIcon} />
-            <div className={styles.waveformBars}>
-                {Array.from({ length: 5 }).map((_, index) => (
-                    <div key={index} className={`${styles.bar} ${recording ? styles.active : ''}`}></div>
-                ))}
-            </div>
         </div>
     );
 };
