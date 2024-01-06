789f9545c72bf1a4cd083411b6b062b54fa5297e
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-06 17:27:47 -0500

    component for waveform style.

---
+++ src/components/waveform.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/waveform.css';
+
+function WaveformLoader() {
+    return (
+        <div className="loader">
+            <span className="stroke"></span>
+            <span className="stroke"></span>
+            <span className="stroke"></span>
+            <span className="stroke"></span>
+            <span className="stroke"></span>
+            <span className="stroke"></span>
+            <span className="stroke"></span>
+        </div>
+    );
+}
+
+export default WaveformLoader;
