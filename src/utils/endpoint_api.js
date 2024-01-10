be57feaff11a96532a14bfde2b2caa56c141a2b4
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-10 15:06:26 -0500

    updated to POST and PATCH requests from GET.

--- src/utils/endpoint_api.js
+++ src/utils/endpoint_api.js
@@ -1,22 +1,56 @@
 
 
-export const fetchProcessedAudio = async () => {
+export async function initialFetchProcessedAudio(file_path) {
     try {
-        const response = await fetch('http://127.0.0.1:8000/audio_endpoint');
+        const requestData = { file_path: file_path };
+        const response = await fetch('http://127.0.0.1:8000/audio_endpoint/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData),
+        });
+
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            throw new Error(`POST request failed with status: ${response.status}`);
         }
+
         const contentType = response.headers.get('content-type');
         if (!contentType || !contentType.includes('application/json')) {
             throw new TypeError("Oops, we haven't got JSON!");
         }
-        const data = await response.json();
-        if (data.floating_point_number !== undefined) {
-            console.log('File Path:', data.file);
-        }
-        return data;
+
+        return await response.json();
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
         return "error"
     }
-};
+}
+
+
+export async function fetchProcessedAudio(file_path, unique_id) {
+    try {
+        const requestData = { file_path: file_path, unique_id: unique_id};
+        const response = await fetch('http://127.0.0.1:8000/audio_endpoint/', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData),
+        });
+
+        if (!response.ok) {
+            throw new Error(`PATCH request failed with status: ${response.status}`);
+        }
+
+        const contentType = response.headers.get('content-type');
+        if (!contentType || !contentType.includes('application/json')) {
+            throw new TypeError("Oops, we haven't got JSON!");
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        return "error"
+    }
+}
