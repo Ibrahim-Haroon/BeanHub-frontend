9f94b263ac3340cd397f84ea98c49396daba0174
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-14 17:45:31 -0500

    make the start and stop recording go based mouse click and no longer trying to analyze audio frequency.

--- src/components/AudioRecorder.js
+++ src/components/AudioRecorder.js
@@ -9,24 +9,22 @@ import '../styles/AudioRecorder.css';
 
 const mimeType = "audio/webm";
 
-const AudioRecorder = () => {
+const AudioRecorder = ({onAudioRecorded}) => {
   const [recordingStatus, setRecordingStatus] = useState(false);
-  const mediaRecorder = useRef(null);
-  const localAudioChunks = useRef([]);
   const mediaStream = useRef(null);
+  const localAudioChunks = useRef([]);
+  const mediaRecorder = useRef(null);
   const audioContext = useRef(new AudioContext());
   const [audioBuffer, setAudioBuffer] = useState(null);
   const [hasPlayedAudio, setHasPlayedAudio] = useState(false);
   const [totalAmount, setTotalAmount] = useState(0);
   const [uniqueId, setUniqueId] = useState(null);
-  let silenceTimer;
 
   useEffect(() => {
     async function initMediaStream() {
       try {
         mediaStream.current = await navigator.mediaDevices.getUserMedia({ audio: true });
         mediaRecorder.current = new MediaRecorder(mediaStream.current, { type: mimeType });
-        startRecording(); // Start recording automatically
       } catch (err) {
         console.error("Error accessing the microphone", err);
       }
@@ -39,21 +37,40 @@ const AudioRecorder = () => {
     };
   }, []);
 
-  const onSpeechResults = () => {
-    console.log("Speech detected");
-    clearTimeout(silenceTimer);
-
-    silenceTimer = setTimeout(() => {
-      if (mediaRecorder.current && mediaRecorder.current.state === "recording") {
-        mediaRecorder.current.stop();
-        console.log("Stopped recording");
-        setRecordingStatus(false);
+  useEffect(() => {
+    const playback = () => {
+      if (audioBuffer && !hasPlayedAudio) { // Check if audio has not been played yet
+        const playSound = audioContext.current.createBufferSource();
+        playSound.buffer = audioBuffer;
+        playSound.connect(audioContext.current.destination);
+        playSound.start(audioContext.current.currentTime);
+        setHasPlayedAudio(true); // Set the flag to prevent further playback
       }
-    }, 1500);
+    };
+
+    playback()
+
+    return () => {
+      window.removeEventListener("playbackEvent", playback);
+    };
+  }, [audioBuffer, hasPlayedAudio]);
+
+  const toggleRecording = () => {
+    if (mediaRecorder.current && mediaRecorder.current.state === 'recording') {
+      stopRecording();
+    } else {
+      setHasPlayedAudio(false); // Reset the flag to allow playback
+      startRecording();
+    }
   };
 
   const startRecording = () => {
-    setRecordingStatus(true);
+    if (mediaRecorder.current && mediaRecorder.current.state !== 'inactive') {
+      // Exit if the recorder is already recording or paused
+      return;
+    }
+
+    setRecordingStatus("recording");
     localAudioChunks.current = [];
     mediaRecorder.current.ondataavailable = (event) => {
       if (event.data.size > 0) {
@@ -65,15 +82,23 @@ const AudioRecorder = () => {
       const audioBlob = new Blob(localAudioChunks.current, { type: mimeType });
       const audioUrl = URL.createObjectURL(audioBlob);
       await convertToWav(audioUrl);
+      onAudioRecorded(audioUrl);
       localAudioChunks.current = [];
     };
 
     mediaRecorder.current.start();
-    onSpeechResults();
   };
 
+  const stopRecording = () => {
+    setRecordingStatus(false);
+    mediaRecorder.current.stop();
+  };
+
+
+  // Convert audio to .wav format
   const convertToWav = async (audioUrl) => {
     try {
+      console.log("Converting to wav")
       const response = await fetch(audioUrl);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -84,41 +109,65 @@ const AudioRecorder = () => {
 
       const wavData = toWav(decodedAudio);
       const wavBlob = new Blob([wavData], { type: 'audio/wav' });
-
+      console.log("Successfully converted to wav and uploaded to S3");
       const uploadFileName = await uploadToS3(wavBlob);
+      console.log("Successfully uploaded to S3 and about to process backend response audio");
       await handleProcessedAudio(uploadFileName);
     } catch (error) {
       console.error('There has been a problem with your fetch operation:', error);
     }
   };
 
+  /*
+   Call backend endpoint to process audio by:
+    1. download res_{unique_id} from s3
+    2. play res_{unique_id} with fetchAndPlayAudio
+    3. delete local copy,
+    4. delete original version on S3
+
+  */
   const handleProcessedAudio = async (uploadFileName) => {
     try {
       let response = null;
       if (!uniqueId) {
+        console.log("Fetching initial processed audio with POST request");
         response = await initialFetchProcessedAudio(uploadFileName);
         setUniqueId(response.unique_id);
       } else {
+        console.log("Fetching processed audio with PATCH request");
         response = await fetchProcessedAudio(uploadFileName, uniqueId);
       }
 
-      if (response && response.file_name && response.json_order) {
-        setTotalAmount(prevTotalAmount => prevTotalAmount + response.json_order['MenuItem']['price']);
-        const tempFilePath = await saveFromS3(response.file_name);
+      console.log("Response from backend: ", response);
+
+      if (response && response.file_path && response.json_order) {
+        console.log("Got valid response from backend");
+        const prices: number[] = response.json_order[0].MenuItem.price;
+        let sum = prices.reduce(function (acc, currentValue) {
+          return acc + currentValue;
+        }, 0);
+        setTotalAmount(prevTotalAmount => prevTotalAmount + sum);
+        console.log("Saving res file from S3");
+        const tempFilePath = await saveFromS3(response.file_path);
+        console.log("Successfully saved res file from S3 and about to decode audio");
         await fetchAndPlayAudio(tempFilePath);
         await deleteTempFile(tempFilePath);
-        await deleteFromS3(response.file_name);
+        await deleteFromS3(response.file_path);
+      } else {
+        console.log("Got invalid response from backend");
       }
     } catch (error) {
       console.error('There has been a problem with your fetch operation:', error);
     }
   };
 
+  // Fetch local copy of audio file decoded into .wav
   const fetchAndPlayAudio = (filePath) => {
     fetch(filePath)
         .then(data => data.arrayBuffer())
         .then(arrayBuffer => {
           try {
+            console.log("Successfully decoded audio");
             return audioContext.current.decodeAudioData(arrayBuffer);
           } catch (error) {
             console.error('Error decoding audio data:', error);
@@ -126,43 +175,25 @@ const AudioRecorder = () => {
           }
         })
         .then(decodedAudio => {
+          console.log("Successfully decoded audio and about to play")
           setAudioBuffer(decodedAudio);
           const event = new Event('playbackEvent');
           window.dispatchEvent(event);
+          setAudioBuffer(null); // Clear the audio buffer
         });
   };
 
-  useEffect(() => {
-    const playback = () => {
-      if (audioBuffer && !hasPlayedAudio) {
-        const playSound = audioContext.current.createBufferSource();
-        playSound.buffer = audioBuffer;
-        playSound.connect(audioContext.current.destination);
-        playSound.start(audioContext.current.currentTime);
-        playSound.onended = () => {
-          setHasPlayedAudio(true);
-          startRecording(); // Restart recording after playback
-        };
-      }
-    };
-
-    playback();
-
-    return () => {
-      window.removeEventListener("playbackEvent", playback);
-    };
-  }, [audioBuffer, hasPlayedAudio]);
 
   return (
-      <div className="audio-recorder-container">
-        <OscillatingCircle />
-        <div className="waveform-loader-container">
-          <WaveformLoader recording={recordingStatus} />
-        </div>
+      <div className="audio-recorder" onClick={toggleRecording} style={{height: '100vh', width: '100vw'}}>
+        <OscillatingCircle/>
         <div className="status-indicator">
           <p>{recordingStatus ? 'Recording...' : 'Not Recording'}</p>
         </div>
-        <OrderTally totalAmount={totalAmount} />
+        <div className="waveform-loader-container">
+          <WaveformLoader recording={recordingStatus}/>
+        </div>
+        <OrderTally totalAmount={totalAmount}/>
       </div>
   );
 };
