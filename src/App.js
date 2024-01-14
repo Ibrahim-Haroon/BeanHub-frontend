1e2b2e12089d83b18ef4eb55d96ac9c1b266f5da
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-14 17:50:32 -0500

    current working version with AudioRecorder.js but will be updated soon.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+// import React, { useState } from 'react';
+// import AudioRecorder from './components/AudioRecorder';
+// import OrderLog from './components/OrderLog';
+// import OrderInputContainer from './components/OrderInputContainer';
+//
+// function App() {
+//     const [totalAmount, setTotalAmount] = useState(100);
+//     return (
+//     <div className="App">
+//         <OrderInputContainer />
+//     </div>
+//     );
+// }
+//
+// export default App;
+//
+
+
 import React, { useState } from 'react';
 import AudioRecorder from './components/AudioRecorder';
-import OrderLog from './components/OrderLog';
-import OrderInputContainer from './components/OrderInputContainer';
+import MicIcon from './components/MicIcon';
+import './App.css';
+
 
 function App() {
-  const [totalAmount, setTotalAmount] = useState(100);
+    const [audioUrl, setAudioUrl] = useState(null);
+    const [isRecording, setIsRecording] = useState(false);
 
-  return (
-    <div className="App">
-      <OrderInputContainer />
-    </div>
-  );
+    const handleAudioRecorded = (audioUrl) => {
+        setAudioUrl(audioUrl);
+        setIsRecording(false);
+    };
+
+    return (
+        <div className="App">
+            <AudioRecorder onAudioRecorded={handleAudioRecorded} />
+        </div>
+    );
 }
 
 export default App;
