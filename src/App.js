b1ee602ad73cb9aafc3ffde809f0edfc4903ed09
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2024-01-14 22:53:55 -0500

    working towards integerating all main components.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
-// import React, { useState } from 'react';
-// import AudioRecorder from './components/AudioRecorder';
-// import OrderLog from './components/OrderLog';
-// import OrderInputContainer from './components/OrderInputContainer';
-//
-// function App() {
-//     const [totalAmount, setTotalAmount] = useState(100);
-//     return (
-//     <div className="App">
-//         <OrderInputContainer />
-//     </div>
-//     );
-// }
-//
-// export default App;
-//
-
-
 import React, { useState } from 'react';
 import AudioRecorder from './components/AudioRecorder';
-import MicIcon from './components/MicIcon';
+import OrderScreen from "./components/OrderScreen";
+import CartManager from "./components/CartManager";
 import './App.css';
 
 
 function App() {
     const [audioUrl, setAudioUrl] = useState(null);
     const [isRecording, setIsRecording] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
 
     const handleAudioRecorded = (audioUrl) => {
         setAudioUrl(audioUrl);
         setIsRecording(false);
     };
 
+    const handleAddItem = (item) => {
+        // Logic to add item to cart
+    };
+
     return (
         <div className="App">
-            <AudioRecorder onAudioRecorded={handleAudioRecorded} />
+            <CartManager setCartItems={setCartItems} addItemToCart={handleAddItem} />
+            <OrderScreen cartItems={cartItems} />
+            <AudioRecorder onAudioRecorded={handleAudioRecorded}/>
         </div>
     );
 }
